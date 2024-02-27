b104d462a07d187abb99974c25ec596ca7b08254
MrPAWAN143 <pkumarrana143@gmail.com>
2024-02-27 17:49:01 +0530

    dynamically images add in manu bar

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const menu = [
     {
-        titile: 'Home', url: '/', dropdown: [{ titile: 'Footbol', img: 'assest/' },
-        { titile: 'Basketball', img: 'assest/' },
-        { titile: 'Volleyball', img: 'assest/' },
-        { titile: 'Rugby', img: 'assest/' },
-        { titile: 'Swimming', img: 'assest/' },
-        { titile: 'Ice-skating', img: 'assest/' },
-        { titile: 'Golf', img: 'assest/' },
-        { titile: 'Skateboard', img: 'assest/' },
-        { titile: 'Snowboard', img: 'assest/' },
-        { titile: 'Hiking', img: 'assest/' },
-        // { titile: 'Tennis', img: 'assest/' },
-        // { titile: 'Cycling', img: 'assest/' },
-        // { titile: 'Gym', img: 'assest/' },
+        titile: 'Home', url: '/', image: 'assest/images/logo.png',
+         dropdown: [
+        { titile: 'Footbol', img: 'assest/images/about-cat-1-sport.png',url:'index.php'},
+        { titile: 'Basketball', img: 'assest/images/about-cat-2-sport.png' ,url:'index.php'},
+        { titile: 'Volleyball', img: 'assest/images/about-cat-3-sport.png',url:'index.php' },
+        { titile: 'Rugby', img: 'assest/images/about-cat-4-sport.png',url:'index.php' },
+        { titile: 'Swimming', img: 'assest/images/about-cat-5-sport.png' ,url:'index.php'},
+        { titile: 'Ice-skating', img: 'assest/images/about-cat-6-sport.png',url:'index.php' },
+        { titile: 'Golf', img: 'assest/images/about-cat-7-sport.png' ,url:'index.php'},
+        { titile: 'Skateboard', img: 'assest/images/about-cat-8-sport.png',url:'index.php' },
+        { titile: 'Snowboard', img: 'assest/images/about-cat-9-sport.png',url:'index.php' },
+        { titile: 'Hiking', img: 'assest/images/about-cat-10-sport.png' ,url:'index.php'},
+            // { titile: 'Tennis', img: 'assest/' },
+            // { titile: 'Cycling', img: 'assest/' },
+            // { titile: 'Gym', img: 'assest/' },
         ], second_dropdown: [
             { titile: 'Latest products', url: '/latestproducts' },
             { titile: 'Sale', url: '/Sale' },
@@ -38,24 +40,36 @@ let nav = document.querySelector('.navbar-nav')
 let all = ''
 
 menu.forEach(function (value, index) {
+    // console.log(value)
+    // let dropdown2 = value.dropdown
+    // console.log(dropdown2)
+
     all += ` <li class="nav-item dropdownx">
     <a href="${value.url}" class="nav-link fs-6">${value.titile}</a>`
-
 
 
     if (value.dropdown != undefined) {
         all += `<div class="dropdown-itemx position-absolute">
         <ul class="menu  navbar-nav flex-row justify-content-between ps-4 pe-4">`
         value.dropdown.forEach(function (items, itemsIndex) {
-            all += `<li class='nav-item'> <a herf='' class='nav-link fs-6' > ${items.titile}</a></li>`
+         
+
+            all += `<div class=''>
+            <a href='${items.url}' class='aa nav-link' >
+            <img src='${items.img}' class='d-block m-auto menuimages'>
+            <li class='nav-item'>
+            
+            <span  class='nav-link fs-6' > ${items.titile}</span>
+                
+            </li> </a> </div>`
 
         })
+        
         all += `</ul></div>`
     }
-
-
     all += `</li >`
 
 })
 
 nav.innerHTML = all
+
